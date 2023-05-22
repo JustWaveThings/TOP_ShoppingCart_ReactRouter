31449d6ebaccd11e5819a971b71ec7f6b8c7ffdc
JustWaveThings <justwavethings@proton.me>
2023-05-21 20:38:19 -0400

    Basic routing

--- TOP_ShoppingCart_ReactRouter/src/App.jsx
+++ TOP_ShoppingCart_ReactRouter/src/App.jsx
@@ -1,13 +1,50 @@
 import React from 'react';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import './meyer.css';
 import './index.css';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Catalog from './components/Catalog';
+import Product from './components/Product';
+import Cart from './components/Cart';
+import NotFound from './components/NotFound';
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route
+			path="/"
+			element={<Layout />}
+		>
+			<Route
+				index
+				element={<Home />}
+			/>
+			<Route
+				path="catalog"
+				element={<Catalog />}
+			>
+				<Route
+					path="catalog/:id"
+					element={<Product />}
+				/>
+			</Route>
+			<Route
+				path="cart"
+				element={<Cart />}
+			/>
+			<Route
+				path="*"
+				element={<NotFound />}
+			/>
+		</Route>
+	),
+	{
+		// basename: 'TOP_ShoppingCart_ReactRouter',
+	}
+);
 
 function App() {
-	return (
-		<div className="test">
-			<h1>Test Test Test</h1>
-		</div>
-	);
+	return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
