409aa0739d78396d6804921967d7199eac938647
JustWaveThings <justwavethings@proton.me>
2023-05-21 20:38:04 -0400

    Basic component structure

---
+++ TOP_ShoppingCart_ReactRouter/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+	return (
+		<div className="header--cont">
+			test
+			<div className="header--title"> Lost It? Buy Now! Lose Later! Buy Again!</div>
+		</div>
+	);
+}
+
+export default Header;
