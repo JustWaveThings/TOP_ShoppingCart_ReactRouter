c64bc8de459d7e5a74af7f645ebfd359b7018fb2
JustWaveThings <justwavethings@proton.me>
2023-05-22 23:17:10 -0400

    another indent test

--- TOP_ShoppingCart_ReactRouter/src/App.jsx
+++ TOP_ShoppingCart_ReactRouter/src/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import './meyer.css';
 import './index.css';
 import Layout from './components/Layout';
@@ -9,41 +14,61 @@ import Product from './components/Product';
 import Cart from './components/Cart';
 import NotFound from './components/NotFound';
 
-const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route
-			path="/"
-			element={<Layout />}
-		>
-			<Route
-				index
-				element={<Home />}
-			/>
-			<Route
-				path="catalog"
-				element={<Catalog />}
-			/>
-			<Route
-				path="catalog/:id"
-				element={<Product />}
-			/>
-			<Route
-				path="cart"
-				element={<Cart />}
-			/>
-			<Route
-				path="*"
-				element={<NotFound />}
-			/>
-		</Route>
-	),
-	{
-		basename: '/TOP_ShoppingCart_ReactRouter',
-	}
-);
+const router =
+  createBrowserRouter(
+    createRoutesFromElements(
+      <Route
+        path='/'
+        element={
+          <Layout />
+        }
+      >
+        <Route
+          index
+          element={
+            <Home />
+          }
+        />
+        <Route
+          path='catalog'
+          element={
+            <Catalog />
+          }
+        />
+        <Route
+          path='catalog/:id'
+          element={
+            <Product />
+          }
+        />
+        <Route
+          path='cart'
+          element={
+            <Cart />
+          }
+        />
+        <Route
+          path='*'
+          element={
+            <NotFound />
+          }
+        />
+      </Route>
+    ),
+    {
+      basename:
+        '/TOP_ShoppingCart_ReactRouter',
+    }
+  );
 
 function App() {
-	return <RouterProvider router={router}></RouterProvider>;
+  return (
+    <RouterProvider
+      router={
+        routser
+      }
+    ></RouterProvider>
+  );
 }
 
 export default App;
