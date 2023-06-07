4da6ea917d3ff2104431a64f8ba4188d959def7f
JustWaveThings <justwavethings@proton.me>
2023-06-06 22:12:11 -0400

    save before trying vitest

--- TOP_ShoppingCart_ReactRouter/vite.config.js
+++ TOP_ShoppingCart_ReactRouter/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/TOP_ShoppingCart_ReactRouter/",
-  plugins: [react()],
-});
+export default defineConfig(
+    {
+        base: '/TOP_ShoppingCart_ReactRouter/',
+        plugins:
+            [
+                react(),
+                '@babel/plugin-syntax-jsx',
+            ],
+    }
+);
